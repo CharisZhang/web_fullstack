a9c62c75e5731615bdf62dc5a91b78c80e6fc3b4
charis <tochariszhang@sina.com>
2019-12-12 20:08:52 +0800

    z

--- cocos/start_project/temp/quick-scripts/assets/scripts/Player.js
+++ cocos/start_project/temp/quick-scripts/assets/scripts/Player.js
@@ -22,10 +22,18 @@ cc.Class({
         jumpHeight: 0,
         // 主角跳跃持续时间
         jumpDuration: 0,
+        // 辅助形变动作时间
+        squashDuration: 0,
         // 最大移动速度
         maxMoveSpeed: 0,
         // 加速度
-        accel: 0
+        accel: 0,
+        // 跳跃音效资源
+        jumpAudio: {
+            default: null,
+            type: cc.AudioClip
+        }
+
         // foo: {
         //     // ATTRIBUTES:
         //     default: null,        // The default value will be used only when the component attaching
@@ -43,17 +51,34 @@ cc.Class({
         // },
     },
     setJumpAction: function setJumpAction() {
+        // var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // // 下落
+        // var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // // 添加一个回调函数，用于在动作结束时调用我们定义的其他方法
+        // var callback = cc.callFunc(this.playJumpSound, this);
+        // // 不断重复，而且每次完成落地动作后调用回调来播放声音
+        // return cc.repeatForever(cc.sequence( jumpUp, jumpDown, callback));
         // 跳跃上升
         var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
         // 下落
         var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
-        // 不断重复
-        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+        // 形变
+        var squash = cc.scaleTo(this.squashDuration, 1, 0.6);
+        var stretch = cc.scaleTo(this.squashDuration, 1, 1.2);
+        var scaleBack = cc.scaleTo(this.squashDuration, 1, 1);
+        // 添加一个回调函数，用于在动作结束时调用我们定义的其他方法
+        var callback = cc.callFunc(this.playJumpSound, this);
+        // 不断重复，而且每次完成落地动作后调用回调来播放声音
+        return cc.repeatForever(cc.sequence(squash, stretch, jumpUp, scaleBack, jumpDown, callback));
+    },
+    playJumpSound: function playJumpSound() {
+        // 调用声音引擎播放声音
+        cc.audioEngine.playEffect(this.jumpAudio, false);
     },
 
     // LIFE-CYCLE CALLBACKS:
     onKeyDown: function onKeyDown(event) {
-        console.log(event);
+        cc.log(event);
         // set a flag when key pressed
         switch (event.keyCode) {
             case cc.macro.KEY.a:
@@ -78,8 +103,11 @@ cc.Class({
     onLoad: function onLoad() {
         this.jumpAction = this.setJumpAction();
         this.node.runAction(this.jumpAction);
+
+        // 加速度方向开关
         this.accLeft = false;
         this.accRight = false;
+        // 主角当前水平方向速度
         this.xSpeed = 0;
 
         // 初始化键盘输入监听
